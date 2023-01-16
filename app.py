426fd33f98a4e91542e6c19126d3a05ab755339f
rodrigomercados <103193611+rodrigomercados@users.noreply.github.com>
2023-01-16 18:52:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import altair as alt
 @st.cache
 def get_UN_data():
   AWS_BUCKET_URL = "https://streamlit-demo-data.s3-us-west-2.amazonaws.com"
-  df = pd.read_csv(AWS_BUCKET_URL + "agri.csv.gz")
+  df = pd.read_csv(AWS_BUCKET_URL + "/agri.csv.gz")
   return df.set_index("Region")
 
 df = get_UN_data()
