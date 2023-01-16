e82b41f8c9029e458139cf63ff945e3c1c6ebb35
rodrigomercados <103193611+rodrigomercados@users.noreply.github.com>
2023-01-16 18:45:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+import streamlit as st
+import pandas as pd
+import altair as alt
 
+@st.cache
+def get_UN_data():
+  AWS_BUCKET_URL = "https://streamlit-demo-data.s3-us-west-2.amazonaws.com"
+  df = pd.read_csv(AWS_BUCKET_URL + "agri.csv.gz")
+  return df.set_index("Region")
+
+df = get_UN_data()
